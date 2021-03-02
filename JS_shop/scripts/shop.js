4e70eaa27e84f90cae742e514b1a2c512e01ca5b
NSLyuma <iinokayo@mail.ru>
2021-03-03 01:05:48 +0300

    Homework for lesson 3

--- JS_shop/scripts/shop.js
+++ JS_shop/scripts/shop.js
@@ -1,7 +1,12 @@
 class AbstractList {
     _items = []
 
-    render() {
+    render(selectorName) {
+        const placeToRender = document.querySelector(selectorName);
+        if (placeToRender) {
+            placeToRender.innerHTML = "";
+        }
+
         this._items.forEach(elem => {
             elem.render();
         });
@@ -9,28 +14,43 @@ class AbstractList {
 }
 
 class List extends AbstractList {
+    _page = 1
+    _CartInstance = null
+
     constructor(CartInstance) {
         super();
-        this.fetchGoods()
-            .then(res => {
-                return res.json()
-                    .then(data => {
-                        const goods = data.data.map(item => {
-                            return new GoodItem(item, CartInstance)
-                        });
-                        this._items = goods;
-                        this.render();
-                    });
+        this._CartInstance = CartInstance;
+        this.initShowMoreButton();
+        this.fetchGoods();
+    }
+
+    initShowMoreButton() {
+        const showMoreButton = document.querySelector("#showmore");
+        if (showMoreButton) {
+            showMoreButton.addEventListener("click", () => {
+                this._page++;
+                this.fetchGoods();
             });
+        }
     }
 
     fetchGoods() {
-        const url = "http://localhost:3000/database/catalog1.json";
-        return fetch(url);
+        const url = `http://localhost:3000/database/catalog${this._page}.json`;
+        return fetch(url)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                const goods = data.data.map(item => {
+                    return new GoodItem(item, this._CartInstance)
+                });
+                this._items = [...this._items, ...goods];
+                this.render();
+            });
     }
 
     render() {
-        super.render();
+        super.render(".goods-list");
     }
 }
 
@@ -95,12 +115,7 @@ class Cart extends AbstractList {
     }
 
     render() {
-        const placeToRender = document.querySelector(".cart-list");
-        if (placeToRender) {
-            placeToRender.innerHTML = "";
-        }
-
-        super.render();
+        super.render(".cart-list");
     }
 
 }
