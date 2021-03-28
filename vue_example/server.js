21eff522f6c997a461c9ced3fd5f3fdad9897494
NSLyuma <iinokayo@mail.ru>
2021-03-28 12:47:00 +0300

    Completed Catalog.vue and CatalogItem.vue

---
+++ vue_example/server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+
+    let body = null;
+
+    try {
+        const ext = req.url.split(".")[1];
+        const isSvg = ext === "svg"
+
+        if (isSvg) {
+            res.setHeader("Content-Type", "image/svg+xml");
+        }
+
+        if (req.url.includes("/img")) {
+            body = fs.readFileSync(`./JS_shop${req.url}`);
+        } else {
+            body = fs.readFileSync(`./JS_shop${req.url}`, "utf8");
+        }
+    } catch (err) {
+        body = fs.readFileSync("./JS_shop/index.html", "utf8");
+    }
+
+    res.end(body);
+});
+
+const port = process.env.PORT || 3000
+
+server.listen(port);
+
+console.log(`Server started on port ${port}!`);
---
+++ vue_example/src/components/ButtonCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="onClick">Clicked {{ counter }} times</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    onClick() {
+      this.counter++;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
